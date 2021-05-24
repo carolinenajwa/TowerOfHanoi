60ee9daafb90fa6924d8a6445ca3afc3dbf1569f
Gavin S <46635587+blue2cat@users.noreply.github.com>
2021-05-24 09:22:03 -0700

    add module layout

--- src/TowerOfHanoi.java
+++ src/TowerOfHanoi.java
@@ -1,12 +1,18 @@
 // Caroline El Jazmi
 // Gavin Stuart
 
-// (Date)
+//this program attempts to solve the classic "Tower of Hanoi" problem.
+
+// 5/25/2021
 
 // ToDo: Game Instructions
 
+//import java utilities
+import java.util.*;
 
-public class TowerofHanoiMain {
+
+public class TowerOfHanoi {
+
     // Num. of disks
     public int numOfDisks;
     // Num of towers
@@ -14,10 +20,23 @@ public class TowerofHanoiMain {
 
     // Run program
     public static void main(String[] args) {
+
+        //new scanner
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the Towers of Hanoi game.");
+        System.out.println("Enter  \"start\" to start moving disks between the rods.");
+        String userInput = sc.next();
+        if (userInput.toLowerCase() == "start") {
+            System.out.println("Starting to transfer disks");
+            //call tower of hanoi
+            TowerOfHanoi();
+        }else {
+            System.out.println("Could not recognize user input; exiting now");
+        }
     }
 
     // Create Array list
-    public TowerofHanoiMain(int numOfDisks) {
+    public void TowerOfHanoi(int numOfDisks) {
         this.numOfDisks = numOfDisks;
         rods = new int[numOfDisks][];
 
@@ -35,27 +54,28 @@ public class TowerofHanoiMain {
     public void moveRod(int numOfDisks, char start, char end, char other) {
         if (numOfDisks == 1) {
             System.out.println("Move disk from rod" + start + " to rod " + end);
-            display();
+            //display(end);
         } else {
             moveRod(numOfDisks - 1, start, end, other);
             moveDisk(start,end);
             moveRod(numOfDisks, other, start, end);
-            display();
+            //display(end);
         }
     }
-    
+
     // Displays moving one disk from starting rod to destination
     private void moveDisk(char start, char end) {
         System.out.println("Move 1 disk from rod" + start + "to rod " + end);
-    }    
-    
-    
+    }
+
+
     // Display towers
     public void display(ArrayList<Integer>[] lists){
-  
-  
-  
-  }
-  
-  
+        for (int i = 0; i < lists.length; i++) {
+            System.out.println(i);
+        }
+    }
+
+
 }
+
